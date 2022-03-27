351c26af04f6af390f7a30e584197ca417448798
haiyang <phycgp@gmail.com>
2022-03-27 19:37:06 +0800

    add 04 node addon exmaple

--- 04/binding.gyp
+++ 04/binding.gyp
@@ -2,7 +2,6 @@
   "targets": [
     {
       "target_name": "tiny_add",
-      "type": "executable",
       "sources": [ "main.cc", "external/add.cc" ],
       "include_dirs": [
         "./external/"
--- 04/main.cc
+++ 04/main.cc
@@ -1,6 +1,23 @@
 #include <cstdio>
+#include <node.h>
+#include <string>
 #include <add.h> // import add from 'add'
 
-int main() {
-  std::printf("1 + 1 = %d", add(1, 1));
+using v8::FunctionCallbackInfo;
+using v8::Isolate;
+using v8::Local;
+using v8::Object;
+using v8::String;
+using v8::Value;
+
+void Method(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  args.GetReturnValue().Set(String::NewFromUtf8(
+      isolate, (std::string("1 + 1 = ") + std::to_string(add(1, 1))).c_str()).ToLocalChecked());
 }
+
+void Initialize(Local<Object> exports) {
+  NODE_SET_METHOD(exports, "tiny_add", Method);
+}
+
+NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
